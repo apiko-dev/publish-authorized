35525c421b5f245521c92dfa960302185765e79c
Taras Dolynyuk <trsdln@gmail.com>
2015-10-22 14:41:19 +0300

    Added github url and description

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'jss:publish-authorized',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Publish only for authorized users or using custom security check',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/JSSolutions/publish-authorized',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
